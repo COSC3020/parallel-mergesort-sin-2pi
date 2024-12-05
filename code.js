898df4d33479358f6695b4a6d13081c547e9ac1b
Nile Young <nilevy03@gmail.com>
2024-12-05 16:53:06 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,29 +1,48 @@
-function mergesort(array) {
-    const arrayLength = array.length;
-    
+const async = require('async');
 
-    for (let subarraySize = 1; subarraySize < arrayLength; subarraySize *= 2) {
-      for (let leftStart = 0; leftStart < arrayLength - 1; leftStart += subarraySize * 2) {
-        const leftEnd = Math.min(leftStart + subarraySize - 1, arrayLength - 1);
-        const rightEnd = Math.min(leftStart + (2 * subarraySize) - 1, arrayLength - 1);
+function mergesort(array) {
+    return new Promise(function(resolve) {
+        arrayLength = array.length;
+        operations = [];
         
-        mergeSubarrays(array, leftStart, leftEnd, rightEnd);
-      }
-    }
-    
-    return array;
-  }
-  
-  function mergeSubarrays(array, leftStart, leftEnd, rightEnd) {
-    let leftIndex = leftStart;
-    let rightIndex = leftEnd + 1;
+        for (let subarraySize = 1; subarraySize < arrayLength; subarraySize *= 2) {
+            operations.push(function(callback) {
+                mergeTasks = [];
+                
+                for (let leftStart = 0; leftStart < arrayLength - 1; leftStart += subarraySize * 2) {
+                    const leftEnd = Math.min(leftStart + subarraySize - 1, arrayLength - 1);
+                    const rightEnd = Math.min(leftStart + (2 * subarraySize) - 1, arrayLength - 1);
+                    
+                    function mergeTask(innerCallback) {
+                        mergeSubarrays(array, leftStart, leftEnd, rightEnd);
+                        innerCallback();
+                    }
+                    
+                    mergeTasks.push(mergeTask);
+                }
+                
+                async.parallel(mergeTasks, function() {
+                    callback();
+                });
+            });
+        }
+        
+        async.series(operations, function() {
+            resolve(array);
+        });
+    });
+}
+
+function mergeSubarrays(array, leftStart, leftEnd, rightEnd) {
+    leftIndex = leftStart;
+    rightIndex = leftEnd + 1;
 
     while (leftIndex < rightIndex && rightIndex <= rightEnd) {
         if (array[leftIndex] <= array[rightIndex]) {
             leftIndex++;
         } else {
             value = array[rightIndex];
-            let index = rightIndex;
+            index = rightIndex;
 
             while (index > leftIndex) {
                 array[index] = array[index - 1];
@@ -36,3 +55,5 @@ function mergesort(array) {
         }
     }
 }
+
+     
